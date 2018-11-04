c6e9622f41dbeafd37d08c71a401d63f6566f0a8
halation-ghost <wpf_practice@elf-mission.net>
2018-11-04 10:21:38 +0900

    episode:4 用ソリューションを追加。

---
+++ 02_episode04/NavigationTree/ViewModels/NavigationTreeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Prism.Mvvm;
+using Reactive.Bindings.Extensions;
+
+namespace WpfTestApp.ViewModels
+{
+	public class NavigationTreeViewModel : BindableBase, System.IDisposable
+	{
+		/// <summary>TreeViewItem を取得します。</summary>
+		public Reactive.Bindings.ReactiveCollection<TreeViewItemViewModel> TreeNodes { get; }
+
+		private WpfTestAppData appData = null;
+		private TreeViewItemViewModel rootNode = null;
+		/// <summary>ReactivePropertyのDispose用リスト</summary>
+		private System.Reactive.Disposables.CompositeDisposable disposables
+			= new System.Reactive.Disposables.CompositeDisposable();
+
+		/// <summary>コンストラクタ。</summary>
+		/// <param name="data">アプリのデータオブジェクト（Unity からインジェクション）</param>
+		public NavigationTreeViewModel(WpfTestAppData data)
+		{
+			this.appData = data;
+			this.rootNode = TreeViewItemCreator.Create(this.appData);
+
+			this.TreeNodes = new Reactive.Bindings.ReactiveCollection<TreeViewItemViewModel>()
+				.AddTo(this.disposables);
+			this.TreeNodes.Add(rootNode);
+		}
+
+		void System.IDisposable.Dispose() { this.disposables.Dispose(); }
+	}
+}
